fab85f7a10284ed65b137327f35d4f0e3fff5e3e
JpRod96 <jprodriguez60@gmail.com>
2020-04-09 15:50:16 -0400

    Database initialization

--- ITSIconta/App.xaml.cs
+++ ITSIconta/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using PersistenceRepositories;
 
 namespace ITSIconta
 {
@@ -30,6 +31,7 @@ namespace ITSIconta
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+            DataAccess.InitializeDatabase();
         }
 
         /// <summary>
---
+++ PersistenceRepositories/DataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using Windows.Storage;
+using System.IO;
+
+namespace PersistenceRepositories
+{
+    public static class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync("ITSIconta.db", CreationCollisionOption.OpenIfExists);
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "ITSIconta.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
